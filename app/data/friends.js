03a6dd3aea27bdd2c7d1c35558a0bdda3d6e6692
Nathaniel Anderson <nathanielmotion@gmail.com>
2020-01-09 10:18:31 -0700

    more image problems

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,18 @@
 var friendMatch = [
   {
-    name: "Aaron Rodgers",
-    photo: "http://static3.businessinsider.com/image/597a40094528e62c008b4ec6/aaron-rodgers-is-structuring-his-life-like-tom-brady-in-order-to-play-football-into-his-40s.jpg ",
+    name: "Red Ranger",
+    photo: "https://store.playstation.com/store/api/chihiro/00_09_000/container/US/en/999/UP2135-CUSA04242_00-AV00000000000061/1574788144000/image?w=240&h=240&bg_color=000000&opacity=100&_version=00_09_000 ",
     scores: ["2", "2", "2", "2", "2"]
   }, {
-    name: "Aaron Judge",
+    name: "Blue Ranger",
     photo: "http://image.nj.com/home/njo-media/width620/img/yankees_main/photo/mlb-baltimore-orioles-at-new-york-yankees-fa5cf650079734ba.jpg ",
     scores: ["3", "3", "3", "3", "3"]
   }, {
-    name: " Alex Morgan",
+    name: " Black Ranger",
     photo: "https://i.ytimg.com/vi/LH8RJ9Q0X0M/maxresdefault.jpg ",
     scores: ["1", "1", "1", "1", "1"]
   }, {
-    name: "Christen Press",
+    name: "Pink Ranger",
     photo: "http://cdn.shopify.com/s/files/1/0661/5963/files/0010_christen_press_large.jpg?9601849381797396010 ",
     scores: ["3", "3", "3", "3", "1"]
   },
