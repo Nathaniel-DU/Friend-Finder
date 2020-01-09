c7a0acf8cf36fc0fd716c6616e11d6858fb00ccd
Nathaniel Anderson <nathanielmotion@gmail.com>
2020-01-09 10:10:56 -0700

    removed images from public because it broke everything

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,19 +1,19 @@
 var friendMatch = [
   {
-    name: "Red Ranger",
-    photo: "images/red-ranger.jpg",
+    name: "Aaron Rodgers",
+    photo: "http://static3.businessinsider.com/image/597a40094528e62c008b4ec6/aaron-rodgers-is-structuring-his-life-like-tom-brady-in-order-to-play-football-into-his-40s.jpg ",
     scores: ["2", "2", "2", "2", "2"]
   }, {
-    name: "Black Ranger",
-    photo: "images/black-ranger.jpg ",
+    name: "Aaron Judge",
+    photo: "http://image.nj.com/home/njo-media/width620/img/yankees_main/photo/mlb-baltimore-orioles-at-new-york-yankees-fa5cf650079734ba.jpg ",
     scores: ["3", "3", "3", "3", "3"]
   }, {
-    name: " Blue Ranger",
-    photo: "images/blue-ranger.jpg",
+    name: " Alex Morgan",
+    photo: "https://i.ytimg.com/vi/LH8RJ9Q0X0M/maxresdefault.jpg ",
     scores: ["1", "1", "1", "1", "1"]
   }, {
-    name: "Pink Ranger",
-    photo: "images/pink-ranger.jpg",
+    name: "Christen Press",
+    photo: "http://cdn.shopify.com/s/files/1/0661/5963/files/0010_christen_press_large.jpg?9601849381797396010 ",
     scores: ["3", "3", "3", "3", "1"]
   },
 
